a5e24790202a8dfc219922c20d17a10df5bd0306
Vita <vitac215@gmail.com>
2017-02-20 00:27:10 -0500

    segment control data flow

--- app/components/SegmentedControl.js
+++ app/components/SegmentedControl.js
@@ -2,50 +2,183 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
-var { SegmentedControlIOS, Text, View, StyleSheet } = ReactNative;
+var { SegmentedControlIOS, StyleSheet, Text, View, ListView, Image } = ReactNative;
 
-export default class SegmentedControl extends React.Component {
-  state = {
-    values: ['Washing', 'Dryer'],
-    selectedIndex: 0
-  };
+import API from '../api';
+import store from '../store';
 
-  _onChange = (event) => {
-    this.setState({
-      selectedIndex: event.nativeEvent.selectedSegmentIndex,
-    });
+var SegmentedControl = React.createClass({
+  getInitialState: function() {
+    console.log(store.getAddress());
+    return {
+      address: store.getAddress(),
+      washingDS: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+      dryerDS: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+      values: ['Washing', 'Dryer'],
+      selectedTab: 'Washing'
+    }
+  },
 
-  };
+  // Fetch data from api
+  componentDidMount: function() {
+    API.getWashingData(this.state.address)
+      .then((res) => {
+        this.setState({
+          washingDS: this.state.washingDS.cloneWithRows(res),
+        })
+      })
+    .then(() => {
+      API.getDryerData(this.state.address)
+        .then((res) => {
+          this.setState({
+            dryerDS: this.state.dryerDS.cloneWithRows(res),
+          })
+        })
+    })
+    .done()
+  },
+
+  renderListView: function() {
+    // Display washing machines
+    if (this.state.selectedTab === 'Washing') {
+      console.log("state is washing");
+      return (
+        <View>
+          {this.renderWashingStatusScene()}
+        </View>
+      )
+    } else if (this.state.selectedTab === 'Dryer') {
+    // Display dryers
+      console.log("state is dryer");
+      return (
+        <View>
+          {this.renderDryerStatusScene()}
+        </View>
+      )
+    }
+  },
+
+  renderWashingStatusScene: function() {
+    console.log("render washing");
+    return (
+      <ListView
+        dataSource = {this.state.washingDS}
+        renderRow = {this.renderRow.bind(this)}
+      />
+    )
+  },
+
+  renderDryerStatusScene: function() {
+    console.log("render dryer");
+    return (
+      <ListView
+        dataSource = {this.state.dryerDS}
+        renderRow = {this.renderRow.bind(this)}
+      />
+    )
+  },
+
+
+  renderRow(rowData) {
+    var img = this.state.selectedTab === 'Washing' ? require('../img/status/Washing.png') : require('../img/status/Dryer.png');
+    return (
+      <View>
+          <View style={styles.rowContainer}>
+              <View style={styles.centerContainer}>
+                  <Text style={[styles.text, styles.machine_id]}>{rowData.machine_id}</Text>
+              </View>
+              <Image style={styles.thumb} source={img} />
+              <View style={styles.textContainer}>
+                  <Text style={[styles.text, styles.remainTime]}>{rowData.remainTime}</Text>
+                  <Text style={[styles.text, styles.endTime]}>{rowData.endTime}</Text>
+                  <Text>{this.state.name}</Text>
+              </View>
+          </View>
+          <View style={styles.separator}/>
+      </View>
+    );
+  },
 
   render() {
     return (
       <View style={styles.container}>
-        <SegmentedControlIOS
-          style={styles.segmentedControl}
-          tintColor='#B0FFFE'
-          values={this.state.values}
-          selectedIndex={this.state.selectedIndex}
-          onChange={this._onChange}/>
+        <View style={styles.scContainer}>
+          <SegmentedControlIOS
+            style={styles.segmentedControl}
+            tintColor='#B0FFFE'
+            values={this.state.values}
+            selectedIndex={0}
+            onValueChange={(val)=> {
+              this.setState({
+                selectedTab: val
+              })
+            }}/>
+        </View>
+        <View style={styles.listContainer}>
+          {this.renderListView()}
+        </View>
       </View>
     );
-  }
+  },
 
-}
+}) // end class
+
 
 var styles = StyleSheet.create({
   container: {
-    backgroundColor: '#4AC3C0'
+    flex: 1
   },
 
-  text: {
-    fontSize: 14,
-    textAlign: 'center',
-    fontWeight: '500',
-    margin: 20,
-    marginBottom: 10
+  scContainer: {
+    backgroundColor: '#4AC3C0',
+    height: 50
+  },
+
+  listContainer: {
+    backgroundColor: '#fff',
+    height: 800
   },
 
   segmentedControl: {
-    margin: 10
+    margin: 10,
+  },
+
+  text: {
+    color: '#929292',
+    fontSize: 15
+  },
+  thumb: {
+    width: 60,
+    height: 60
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  rowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 15
+  },
+  centerContainer: {
+    justifyContent: 'center'
+  },
+  machine_id: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginRight: 20
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: 'flex-end'
+  },
+  remainTime: {
+    fontSize: 30
+  },
+  endTime: {
+    fontSize: 15,
+    fontWeight: 'bold'
   }
 });
+
+module.exports = SegmentedControl;
--- app/scenes/StatusScene.js
+++ app/scenes/StatusScene.js
@@ -5,49 +5,16 @@ import React, { Component } from 'react';
 import { Text, TextInput, StyleSheet, Image, ScrollView, ListView, View } from 'react-native';
 
 import SegmentedControl from '../components/SegmentedControl';
-// function saveData() {
-//     AsyncsStorage.setItem("name", "Test");
-//     this.setState("name", "Test");
-// }
 
 export default class StatusScene extends Component {
   constructor(props) {
     super(props);
-    var ds = new ListView.DataSource(
-      {rowHasChanged: (r1, r2) => r1 !== r2});
-    var data = [{'machine_id': 1, 'remainTime':'00:30', 'endTime':'8:30', 'status': 'In Use'}, {'machine_id': 2, 'remainTime':'00:20', 'endTime':'8:20', 'status': 'In Use'}, {'machine_id': 3, 'remainTime':'00:10', 'endTime':'8:10', 'status': 'In Use'}];
-    this.state = {
-      dataSource: ds.cloneWithRows(data),
-      machineType: 'washing'
-    };
-  }
-
-  renderRow(rowData) {
-    return (
-        <View>
-            <View style={styles.rowContainer}>
-                <View style={styles.centerContainer}>
-                    <Text style={[styles.text, styles.machine_id]}>{rowData.machine_id}</Text>
-                </View>
-                <Image style={styles.thumb} source={ require('../img/washing.png') } />
-                <View style={styles.textContainer}>
-                    <Text style={[styles.text, styles.remainTime]}>{rowData.remainTime}</Text>
-                    <Text style={[styles.text, styles.endTime]}>{rowData.endTime}</Text>
-                    <Text>{this.state.name}</Text>
-                </View>
-            </View>
-            <View style={styles.separator}/>
-        </View>
-    );
   }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <SegmentedControl />
-        <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}/>
       </View>
     );
   }
@@ -55,46 +22,7 @@ export default class StatusScene extends Component {
 }
 
 var styles = StyleSheet.create({
-  text: {
-    color: '#929292',
-    fontSize: 15
-  },
   container: {
-    flex: 1,
-    padding: 10
-  },
-  thumb: {
-    width: 60,
-    height: 55,
-    marginRight: 15
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#dddddd'
-  },
-  rowContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 18
-  },
-  centerContainer: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  machine_id: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginRight: 20
-  },
-  textContainer: {
-    flex: 1,
-    alignItems: 'flex-end'
-  },
-  remainTime: {
-    fontSize: 30
-  },
-  endTime: {
-    fontSize: 15,
-    fontWeight: 'bold'
+    flex: 1
   }
 });
